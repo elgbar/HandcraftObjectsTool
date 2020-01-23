ae0e8424a7a02e0b3ade8e82d5d6447ed933b592
Karl Henrik Elg <klink100@gmail.com>
2020-01-23 11:19:41 +0100

    Add convertion method to SerializationManager

--- api/src/main/kotlin/no/uib/inf219/api/serialization/SerializationManager.kt
+++ api/src/main/kotlin/no/uib/inf219/api/serialization/SerializationManager.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema
+import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 
 
@@ -26,6 +28,10 @@ object SerializationManager {
         )
     }
 
+    fun generateSchema(clazz: Class<*>): JsonSchema {
+        return JsonSchemaGenerator(mapper).generateSchema(clazz)
+    }
+
     /**
      * Convert the given object to YAML
      *
@@ -37,12 +43,36 @@ object SerializationManager {
     }
 
     /**
-     * Load an object from YAML
+     * Shorthand for `dump(loadFromMap(map))`
      *
-     * @return An instance of [T] with the properties of the given YAML
+     * @return An instance of [T] with the properties of the given map
+     */
+    @JvmStatic
+    inline fun <reified T> dumpMap(map: Map<String, Any?>): String {
+        return dump(loadFromMap(map))
+    }
+
+    /**
+     * Load an object from string
+     *
+     * @return An instance of [T] with the properties of the given string
      */
     @JvmStatic
     inline fun <reified T> load(str: String): T {
         return mapper.readValue(str, T::class.java)
+    }
+
+    /**
+     * Load an object from map
+     *
+     * @return An instance of [T] with the properties of the given map
+     */
+    @JvmStatic
+    inline fun <reified T> loadFromMap(map: Map<String, Any?>): T {
+        return mapper.convertValue(map, T::class.java)
+    }
+
+    fun loadFromMap(map: Map<String, Any?>, clazz: Class<*>): Any {
+        return mapper.convertValue(map, clazz)
     }
 }
