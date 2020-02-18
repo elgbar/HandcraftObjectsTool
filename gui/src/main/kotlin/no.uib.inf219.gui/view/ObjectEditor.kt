f6cb31d0552dc6e676f89e26fe0b82492776ad43
Karl Henrik Elg <klink100@gmail.com>
2020-02-18 13:17:10 +0100

    Add save button to save edited objects

--- gui/src/main/kotlin/no.uib.inf219.gui/view/ObjectEditor.kt
+++ gui/src/main/kotlin/no.uib.inf219.gui/view/ObjectEditor.kt
@@ -1,7 +1,9 @@
 package no.uib.inf219.gui.view
 
+import javafx.stage.FileChooser
 import no.uib.inf219.gui.Styles
 import no.uib.inf219.gui.controllers.ObjectEditorController
+import no.uib.inf219.gui.view.ControlPanelView.mapper
 import tornadofx.*
 
 
@@ -34,33 +36,72 @@ class ObjectEditor(private val controller: ObjectEditorController) : View() {
         bottom = hbox {
             addClass(Styles.parent)
 
-            button("Validate") {
+            button("Save") {
                 setOnAction {
-                    OutputArea.logln("Validating...")
-                    runAsync {
-                        try {
-                            val obj = controller.rootBuilder.toObject()
-                            ui {
-                                if (obj == null) {
-                                    OutputArea.logln("Object created without error, but it is null")
-                                } else {
-                                    OutputArea.logln("Successfully created object!")
-                                    OutputArea.logln("obj=$obj")
-                                    OutputArea.logln("json=${ControlPanelView.mapper.writeValueAsString(obj)}")
-                                }
-                            }
-                        } catch (e: Throwable) {
-                            ui {
-                                OutputArea.logln("Failed to create object due to")
-                                OutputArea.logln(e.toString())
-                            }
-                        }
+
+                    val obj = toObject()
+                    if (obj == null) {
+                        OutputArea.logln("Cannot save object as it is not valid. Please validate it first")
+                        return@setOnAction
+                    }
+
+                    val files = chooseFile(
+                        "Choose where to save",
+                        arrayOf(
+                            FileChooser.ExtensionFilter(
+                                "Common types",
+                                "*.json",
+                                "*.yaml",
+                                "*.yml",
+                                "*.txt",
+                                "*.csv",
+                                "*.properties",
+                                "*.xml"
+                            ),
+                            FileChooser.ExtensionFilter("All files", "*")
+                        ),
+                        FileChooserMode.Save
+                    )
+
+                    if (files.isEmpty()) return@setOnAction
+                    val file = files[0]
+
+                    ControlPanelView.runAsync {
+                        mapper.writeValue(file, obj)
+                        OutputArea.logln("Saved object to file ${file.canonicalPath}")
                     }
                 }
             }
 
+            button("Validate") {
+                setOnAction {
+                    OutputArea.logln("Validating...")
+                    runAsync {
+
+                        val obj = toObject()
+                        if (obj == null) {
+                            OutputArea.logln("Object created without error, but it is null")
+                        } else {
+                            OutputArea.logln("Successfully created object!")
+                            OutputArea.logln("obj=$obj")
+                            OutputArea.logln("json=${mapper.writeValueAsString(obj)}")
+                        }
+
+                    }
+                }
+            }
             this += OutputArea.clearButton()
         }
+    }
+
+    fun toObject(): Any? {
+        try {
+            return controller.rootBuilder.toObject()!!
+        } catch (e: Throwable) {
+            OutputArea.logln("Failed to create object due to")
+            OutputArea.logln(e.toString())
+        }
+        return null
     }
 }
 
