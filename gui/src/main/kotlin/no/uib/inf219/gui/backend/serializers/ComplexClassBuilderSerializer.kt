6772523d6efd1704706cb1c442e1c96df71b1396
Karl Henrik Elg <klink100@gmail.com>
2020-03-06 17:04:11 +0100

    Fix recursive object not working
    Note that this does kinda break more complex objects with id confusion

--- gui/src/main/kotlin/no/uib/inf219/gui/backend/serializers/ComplexClassBuilderSerializer.kt
+++ gui/src/main/kotlin/no/uib/inf219/gui/backend/serializers/ComplexClassBuilderSerializer.kt
@@ -2,11 +2,15 @@ package no.uib.inf219.gui.backend.serializers
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.jsontype.TypeSerializer
+import com.fasterxml.jackson.databind.ser.BeanSerializer
+import com.fasterxml.jackson.databind.ser.impl.ObjectIdWriter
 import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import no.uib.inf219.extra.type
 import no.uib.inf219.gui.backend.ComplexClassBuilder
+import tornadofx.findFieldByName
 
 /**
  * @author Elg
@@ -15,7 +19,6 @@ class ComplexClassBuilderSerializer : StdSerializer<ComplexClassBuilder<*>>(Comp
 
 
     override fun serialize(value: ComplexClassBuilder<*>, gen: JsonGenerator, provider: SerializerProvider) {
-        //
         serializeWithType(value, gen, provider, value.typeSerializer)
     }
 
@@ -26,15 +29,58 @@ class ComplexClassBuilderSerializer : StdSerializer<ComplexClassBuilder<*>>(Comp
         typeSer: TypeSerializer?
     ) {
 
-        gen.writeStartObject()
+        val beanSer = provider.findValueSerializer(value.type) as BeanSerializer
+        val objIdWriter: ObjectIdWriter? = beanSer.javaClass.findFieldByName("_objectIdWriter").also {
+            it?.isAccessible = true
+        }?.get(beanSer) as ObjectIdWriter?
 
+        if (objIdWriter != null) {
+
+            val ser = objIdWriter.serializer ?: provider.findValueSerializer(objIdWriter.idType)
+
+            val serObjIdWriter = objIdWriter.withSerializer(ser)
+
+            checkNotNull(serObjIdWriter.serializer)
+
+            val objectId = provider.findObjectId(value.serObject, serObjIdWriter.generator)
+            // If possible, write as id already
+            if (objectId.writeAsId(gen, provider, serObjIdWriter)) {
+                return
+            }
+            // If not, need to inject the id:
+            val id = objectId.generateId(value.serObject)
+
+            if (serObjIdWriter.alwaysAsId) {
+                serObjIdWriter.serializer.serialize(id, gen, provider)
+                return
+            }
+
+            gen.writeStartObject()
+            objectId.writeAsField(gen, provider, serObjIdWriter)
+
+        } else {
+            gen.writeStartObject()
+        }
+
+        //include type information as a new property
         if (typeSer?.typeInclusion == JsonTypeInfo.As.PROPERTY) {
             gen.writeStringField(typeSer.propertyName, value.type.rawClass.canonicalName)
         }
 
-        for ((field, obj) in value.serObject) {
-            gen.writeObjectField(field, obj)
+        for ((key, prop) in value.serObject) {
+
+            // Null handling is bit different, check that first
+            if (prop == null) {
+                gen.writeNullField(key)
+                break
+            }
+            // then find serializer to use
+            val ser: JsonSerializer<Any> = provider.findValueSerializer(prop::class.java)
+
+            gen.writeFieldName(key)
+            ser.serialize(prop, gen, provider)
         }
+
         gen.writeEndObject()
     }
 }
