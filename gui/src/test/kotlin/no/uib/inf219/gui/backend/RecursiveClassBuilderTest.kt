d5fb59ca8ce74a154aa0364ecfe449334a97b962
Karl Henrik Elg <klink100@gmail.com>
2020-03-06 17:02:55 +0100

    Fix deserialization of UselessRecursiveObject

--- gui/src/test/kotlin/no/uib/inf219/gui/backend/RecursiveClassBuilderTest.kt
+++ gui/src/test/kotlin/no/uib/inf219/gui/backend/RecursiveClassBuilderTest.kt
@@ -3,7 +3,8 @@ package no.uib.inf219.gui.backend
 import no.uib.inf219.extra.type
 import no.uib.inf219.gui.view.ControlPanelView
 import no.uib.inf219.test.UselessRecursiveObject
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.assertj.core.api.Assertions.fail
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.extension.ExtendWith
@@ -17,7 +18,7 @@ class RecursiveClassBuilderTest {
 
     @Test
     internal fun canSerialiseRecursiveClass() {
-        val rec = UselessRecursiveObject(null)
+        val rec = UselessRecursiveObject()
         rec.with = rec
 
         var json: String? = null
@@ -28,18 +29,37 @@ class RecursiveClassBuilderTest {
     }
 
     @Test
+    internal fun canDeserializeRecursiveClass() {
+        val json = "{\"@id\":1,\"with\":1}"
+
+        val made: UselessRecursiveObject
+        try {
+            made = ControlPanelView.mapper.readValue(json, UselessRecursiveObject::class.java)
+        } catch (e: Throwable) {
+            fail<Any>("Could not read from json", e)
+            return
+        }
+
+        val rec = UselessRecursiveObject()
+        rec.with = rec
+
+        assertTrue(made === made.with)
+    }
+
+    @Test
     internal fun canCreateRecursiveClass() {
-        val rec = UselessRecursiveObject(null)
+        val rec = UselessRecursiveObject()
         rec.with = rec
 
         val cb = ComplexClassBuilder<UselessRecursiveObject>(UselessRecursiveObject::class.type(), "rec")
-        cb.serObject[UselessRecursiveObject::with.name] = ReferenceClassBuilder(cb, null)
+        cb.serObject[UselessRecursiveObject::with.name] = cb
 
         var created: UselessRecursiveObject? = null
         assertDoesNotThrow {
             created = cb.toObject()
         }
 
-        assertEquals(rec, created)
+        assertNotNull(created)
+        assertTrue(created === created!!.with)
     }
 }
--- gui/src/test/kotlin/no/uib/inf219/test/UselessRecursiveObject.kt
+++ gui/src/test/kotlin/no/uib/inf219/test/UselessRecursiveObject.kt
@@ -1,10 +1,15 @@
 package no.uib.inf219.test
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.ObjectIdGenerators
 
 /**
  * @author Elg
  */
 @JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator::class)
-class UselessRecursiveObject(var with: UselessRecursiveObject?) {}
+class UselessRecursiveObject {
+
+    @JsonProperty("with")
+    var with: UselessRecursiveObject? = null
+}
