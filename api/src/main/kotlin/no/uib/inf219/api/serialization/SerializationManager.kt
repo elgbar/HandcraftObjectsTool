f3f1d808b800d49350b0d3fcc5726390852bf213
Karl Henrik Elg <klink100@gmail.com>
2020-03-16 14:14:16 +0100

    Remove unused dependency

--- api/src/main/kotlin/no/uib/inf219/api/serialization/SerializationManager.kt
+++ api/src/main/kotlin/no/uib/inf219/api/serialization/SerializationManager.kt
@@ -2,10 +2,8 @@ package no.uib.inf219.api.serialization
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.apache.commons.lang.WordUtils
-
 
 /**
  * A series of different [ObjectMapper] considered to be default in the HOT system
@@ -100,10 +98,4 @@ object SerializationManager {
     inline fun <reified T> ObjectMapper.readValue(str: String): T {
         return this.readValue(str, T::class.java)
     }
-}
-
-
-fun ObjectMapper.schema(clazz: Class<*>): String {
-    val schemaGen = JsonSchemaGenerator(this)
-    return writeValueAsString(schemaGen.generateSchema(clazz))
 }
