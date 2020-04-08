358f00c4e494a93b5f602a7dcd011a00eaa8ccc8
Karl Henrik Elg <klink100@gmail.com>
2020-04-08 20:29:33 +0200

    Add icon

--- gui/src/main/kotlin/no/uib/inf219/gui/GuiMain.kt
+++ gui/src/main/kotlin/no/uib/inf219/gui/GuiMain.kt
@@ -1,5 +1,6 @@
 package no.uib.inf219.gui
 
+import javafx.stage.Stage
 import no.uib.inf219.gui.view.BackgroundView
 import tornadofx.App
 
@@ -10,5 +11,12 @@ class GuiMain : App(BackgroundView::class, Styles::class) {
 
     companion object {
         const val FILES_FOLDER = "classes"
+    }
+
+    override fun start(stage: Stage) {
+        stage.icons += resources.image("/icon/chisel-256.png")
+        stage.icons += resources.image("/icon/chisel-128.png")
+        stage.icons += resources.image("/icon/chisel-64.png")
+        super.start(stage)
     }
 }
