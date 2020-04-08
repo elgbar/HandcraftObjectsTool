6b47e9767a70aea56a1ad744af3b2c1212860adb
Karl Henrik Elg <klink100@gmail.com>
2020-04-08 17:28:21 +0200

    Add method to create centered text

--- gui/src/main/kotlin/no/uib/inf219/extra/EventTarget.kt
+++ gui/src/main/kotlin/no/uib/inf219/extra/EventTarget.kt
@@ -2,8 +2,13 @@ package no.uib.inf219.extra
 
 import javafx.beans.property.StringProperty
 import javafx.event.EventTarget
+import javafx.geometry.Pos
+import javafx.scene.layout.VBox
+import javafx.scene.text.TextAlignment
 import no.uib.inf219.gui.backend.ClassBuilder
 import tornadofx.text
+import tornadofx.textflow
+import tornadofx.vbox
 
 /**
  * Create a text object that is dependent on the given class builder.
@@ -20,5 +25,21 @@ fun EventTarget.textCb(cb: ClassBuilder, value: ClassBuilder.() -> String) {
 fun StringProperty.bindCbText(cb: ClassBuilder, value: ClassBuilder.() -> String) {
     cb.serObjectObservable.onChange {
         this.set(value(cb))
+    }
+}
+
+
+fun EventTarget.centeredText(vararg lines: String, op: VBox.() -> Unit = {}) {
+    vbox {
+        alignment = Pos.CENTER
+        textflow {
+            textAlignment = TextAlignment.CENTER
+            for (line in lines.dropLast(1)) {
+                text(line + "\n")
+            }
+            //do not add a newline to the last element
+            text(lines.last())
+        }
+        op()
     }
 }
