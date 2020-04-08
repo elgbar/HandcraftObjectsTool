9bba140735e8b6b104de39c4f1e8f6c12a4b6fed
Karl Henrik Elg <klink100@gmail.com>
2020-04-08 19:07:23 +0200

    Add about page

--- gui/src/main/kotlin/no/uib/inf219/gui/view/BackgroundView.kt
+++ gui/src/main/kotlin/no/uib/inf219/gui/view/BackgroundView.kt
@@ -3,8 +3,12 @@ package no.uib.inf219.gui.view
 import javafx.geometry.Orientation
 import javafx.scene.control.TabPane
 import javafx.scene.layout.BorderPane
+import javafx.scene.paint.Color
+import no.uib.inf219.extra.centeredText
 import no.uib.inf219.extra.close
 import no.uib.inf219.extra.closeAll
+import no.uib.inf219.extra.internetHyperlink
+import no.uib.inf219.gui.Styles
 import no.uib.inf219.gui.ems
 import tornadofx.*
 
@@ -51,6 +55,41 @@ class BackgroundView : View("Handcrafted Objects Tool") {
                         val oebv = ControlPanelView.tabMap[tab] ?: return@action
 
                         oebv.save()
+                    }
+                }
+
+                menu("Help") {
+                    item("About").action {
+
+                        object : View("About HOT") {
+                            override val root = borderpane {
+                                style {
+                                    backgroundColor = multi(Color.WHITE)
+                                }
+                                addClass(Styles.parent)
+
+                                top {
+                                    centeredText("Handcraft Objects Tool") {
+                                        addClass(Styles.headLineLabel)
+                                    }
+                                }
+
+                                center {
+                                    centeredText(
+                                        "Open source tool to create JVM objects with Jackson",
+                                        "Jackson is licenced under Apache License 2.0",
+                                        "Author: Karl Henrik Elg Barlinn"
+                                    ) {
+                                        addClass(Styles.parent)
+
+                                        textflow {
+                                            text("Github: ")
+                                            internetHyperlink("https://github.com/kh498/HandcraftObjectsTool")
+                                        }
+                                    }
+                                }
+                            }
+                        }.openModal(block = true, owner = currentWindow)
                     }
                 }
             }
