27cc0f1d9075be9712f00ccca0b36e7233973106
Karl Henrik Elg <klink100@gmail.com>
2020-04-08 17:22:49 +0200

    Fire close event when calling close methods

--- gui/src/main/kotlin/no/uib/inf219/extra/Pane.kt
+++ gui/src/main/kotlin/no/uib/inf219/extra/Pane.kt
@@ -1,5 +1,6 @@
 package no.uib.inf219.extra
 
+import javafx.event.Event
 import javafx.scene.control.Tab
 import javafx.scene.control.TabPane
 
@@ -14,8 +15,15 @@ fun TabPane.closeAll(force: Boolean = false) {
     val iter = this.tabs.iterator()
     while (iter.hasNext()) {
         val tab = iter.next()
-        if (force || tab.isClosable)
+        if (force || tab.isClosable) {
+            Event.fireEvent(tab, Event(Tab.CLOSED_EVENT))
             iter.remove() // same as 'tab.close()'
+        }
     }
+}
+
+fun Tab.close() {
+    Event.fireEvent(this, Event(Tab.CLOSED_EVENT))
+    tabPane?.tabs?.remove(this)
 }
 
