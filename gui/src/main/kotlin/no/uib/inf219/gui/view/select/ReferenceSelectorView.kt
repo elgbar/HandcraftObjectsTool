416dc62e1b748dca94a9aa2100753cae8c86cec7
Elg <klink100@gmail.com>
2020-05-08 12:24:46 +0200

    Find longer cycles and do not show them when selecting references

--- gui/src/main/kotlin/no/uib/inf219/gui/view/select/ReferenceSelectorView.kt
+++ gui/src/main/kotlin/no/uib/inf219/gui/view/select/ReferenceSelectorView.kt
@@ -2,8 +2,10 @@ package no.uib.inf219.gui.view.select
 
 import com.fasterxml.jackson.databind.JavaType
 import no.uib.inf219.extra.findChild
+import no.uib.inf219.extra.isTypeOrSuperTypeOfPrimAsObj
 import no.uib.inf219.gui.backend.cb.api.ClassBuilder
 import no.uib.inf219.gui.backend.cb.api.ParentClassBuilder
+import no.uib.inf219.gui.backend.cb.node
 import no.uib.inf219.gui.backend.cb.path
 import no.uib.inf219.gui.backend.cb.reference.ReferenceClassBuilder
 import no.uib.inf219.gui.controllers.cbn.FilledClassBuilderNode
@@ -14,7 +16,6 @@ import no.uib.inf219.gui.controllers.cbn.FilledClassBuilderNode
 class ReferenceSelectorView : SelectorView<ClassBuilder>("Reference") {
 
     override val promptText = "Class builder name"
-
 
     override fun cellText(elem: ClassBuilder) = "${elem.getPreviewValue()} [${elem.path}]"
 
@@ -27,30 +28,44 @@ class ReferenceSelectorView : SelectorView<ClassBuilder>("Reference") {
         key: ClassBuilder,
         parent: ParentClassBuilder
     ): ReferenceClassBuilder? {
+
+        //true if no cycle
+        fun ClassBuilder.checkNoCycle(): Boolean {
+            val currSerObject = serObject
+            //Not ref -> not cycle
+            if (this !is ReferenceClassBuilder) return true
+
+            //selected ser
+            else if (currSerObject === parent[key] ||
+                this.refParent === parent && this.refKey.serObject == key.serObject
+            ) return false
+            
+            //might be a longer cycle
+            if (currSerObject is ReferenceClassBuilder) return currSerObject.checkNoCycle()
+            return true
+        }
+
         result = null
         searching = true
         searchResult.clear()
-        tornadofx.runAsync {
-            searchResult.setAll(
-                findInstancesOf(
-                    type,
-                    controller.root
-                ).filter { it != parent[key] })
-            searching = false
-        }
+        val currSerObj = parent[key]
+        searchResult.setAll(
+            findInstancesOf(
+                type,
+                controller.root
+            ).filter {
+                it !== currSerObj && (currSerObj == null || it.checkNoCycle())
+
+            })
+        searching = false
+
         openModal(block = true, owner = currentWindow, escapeClosesWindow = false)
 
         val ref = result ?: return null
         val item = parent.item.findChild(key)
         require(item.value.allowReference) { "Somehow you selected a value that does not allow references! $ref" }
 
-        return ReferenceClassBuilder(
-            ref.key,
-            ref.parent,
-            key,
-            parent,
-            item
-        ).also {
+        return ReferenceClassBuilder(ref.key, ref.parent, key, parent, item).also {
             item.value = FilledClassBuilderNode(key, it, parent, item, true)
         }
     }
@@ -73,9 +88,9 @@ class ReferenceSelectorView : SelectorView<ClassBuilder>("Reference") {
             }
 
             //find all children that is the correct type
-            // and isn't a ReferenceClassBuilder to prevent cycles
-            return allChildren.filter { it.type.isTypeOrSubTypeOf(wantedType.rawClass) && it.node.allowReference }
-                .toSet()
+            return allChildren.filter {
+                it.type.isTypeOrSuperTypeOfPrimAsObj(wantedType.rawClass) && it.node.allowReference
+            }.toSet()
         }
     }
 }
