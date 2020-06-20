dd8699cbd0629b1978042bcac32a87ddfb5e3680
Karl Henrik Elg <klink100@gmail.com>
2020-06-20 13:52:22 +0200

    Add method to ensure a File is a folder

--- gui/src/main/kotlin/no/uib/inf219/extra/File.kt
+++ gui/src/main/kotlin/no/uib/inf219/extra/File.kt
@@ -1,6 +1,8 @@
 package no.uib.inf219.extra
 
+import javafx.scene.control.ButtonType
 import java.io.*
+import kotlin.system.exitProcess
 
 /**
  * @author Elg
@@ -38,4 +40,38 @@ fun File.copyInputStreamToFile(inputStream: InputStream) {
     this.outputStream().use { fileOut ->
         inputStream.copyTo(fileOut)
     }
+}
+
+val DELETE = ButtonType("Delete")
+val RENAME = ButtonType("Rename")
+val EXIT = ButtonType("Exit application")
+
+/**
+ * Make sure this file is a folder. If it is not the user will be asked what will be done.
+ * Either delete the file, rename or exit the application
+ *
+ * @return this, for chaining
+ */
+fun File.ensureFolder(): File {
+    if (this.isFile) {
+        tornadofx.error(
+            "The file at '$this' is a file, the application requires this to be a folder.",
+            "To fix this choose an action, default to delete.",
+            owner = null,
+            buttons = *arrayOf(DELETE, RENAME, EXIT),
+            actionFn = {
+                when (it) {
+                    DELETE -> this@ensureFolder.deleteRecursively()
+                    RENAME -> this@ensureFolder.renameTo(this@ensureFolder.resolveSibling("${this@ensureFolder.name}-file"))
+                    EXIT -> exitProcess(1)
+                }
+            }
+        )
+    }
+
+    if (!exists() && !mkdirs()) {
+        tornadofx.error("Failed to create folder '$this'. Application will exit")
+        exitProcess(1)
+    }
+    return this
 }
